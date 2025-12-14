a5cd3a25242b251bb889df2ad4101b3240345e99
navaneethhhk <hknavaneeth@gmail.com>
2025-12-14 22:34:08 +0530

    add all

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,24 +1,24 @@
  const {test, expect} = require('@playwright/test');
  const {POManager} = require('../pageObjectModel/POManager');
-
+const dataset=JSON.parse(JSON.stringify(require('../utils/productsTestData.json')));
 
  test('Client App login', async ({page})=>
  {
    const poManager = new POManager(page);
     //js file- Login js, DashboardPage
-     const username = "hknavaneeth@gmail.com";
-     const password = "Nithu@12345"
-     const productName = 'Zara Coat 4';
+    //  const username = "hknavaneeth@gmail.com";
+    //  const password = "Nithu@12345"
+    //  const productName = 'Zara Coat 4';
      const products = page.locator(".card-body");
      const loginPage = poManager.getLoginPage();
      await loginPage.goTo();
-     await loginPage.validLogin(username,password);
+     await loginPage.validLogin(dataset.username,dataset.password);
      const dashboardPage = poManager.getDashboardPage();
-     await dashboardPage.searchProductAddCart(productName);
+     await dashboardPage.searchProductAddCart(dataset.productName);
      await dashboardPage.navigateToCart();
 
     const cartPage = poManager.getCartPage();
-    await cartPage.VerifyProductIsDisplayed(productName);
+    await cartPage.VerifyProductIsDisplayed(dataset.productName);
     await cartPage.Checkout();
 
     const ordersReviewPage = poManager.getOrdersReviewPage();
@@ -29,39 +29,6 @@
    const ordersHistoryPage = poManager.getOrdersHistoryPage();
    await ordersHistoryPage.searchOrderAndSelect(orderId);
    expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-    
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
 
 
  });
