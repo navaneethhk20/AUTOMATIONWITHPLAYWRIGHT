ac57cdea6b122a97c11967d2b9029834cc2ff064
navaneethhhk <hknavaneeth@gmail.com>
2025-12-14 21:55:17 +0530

    add all

--- tests/MoreFunctionalities.spec.js
+++ tests/MoreFunctionalities.spec.js
@@ -1,6 +1,6 @@
 const {test,expect}=require('@playwright/test')
 
-test.only('more functionalities',async({page})=>
+test('more functionalities',async({page})=>
 {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     // await page.goto("https://google.com/");
@@ -20,4 +20,20 @@ test.only('more functionalities',async({page})=>
     await framepage.locator("div a[href*='all-access-subscription']").first().click();
     const users= await framepage.locator('div .text-2xl').nth(1).textContent();
     console.log(users);
+});
+
+test('screenshots and visual checks',async({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator('#displayed-text')).toBeVisible();
+    await page.locator('#hide-textbox').click();
+    await page.screenshot({path:'screenshot2.png'});
+    await expect (page.locator('#displayed-text')).toBeHidden();
+    await page.locator('#displayed-text').screenshot({path:'partialscreenshot2.png'});
+});
+
+test.only('visual', async({page})=>
+{
+    page.goto("https://www.facebook.com/");
+    expect(await page.screenshot()).toMatchSnapshot('landing.png');
 });
